d457b1b5a878454c144698bcd780a19aab1fe8c8
Doudou <adeldoudou1996@gmail.com>
2019-09-10 11:13:00 +0200

    Update testpoo.php

--- model/testpoo.php
+++ model/testpoo.php
@@ -1,18 +1,21 @@
 <?php
-
-function autoload($class)
+function chargerClasse($class)
 {
-	require $class . '.php';
+  require $class . '.php'; // On inclut la classe correspondante au paramètre passé.
 }
 
-$admin = new Administrator([
+spl_autoload_register('chargerClasse'); // On enregistre la fonction en autoload pour qu'elle soit appelée dès qu'on instanciera une classe non déclarée.
+
+$administrator = new Administrator([
 	'email' => 'phil@gmail.com',
 	'name' => 'phil',
 	'firstName' => 'Me',
 	'password' => 'yoyoyoyo'
 ]);
 
-$db = new PDO('mysql:host=localhost;dbname=POO', 'root', '');
+$db = new PDO('mysql:host=localhost;dbname=POO;charset=utf8', 'root', 'root');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
 $manager = new AdministratorManager($db);
 
-$manager->add($admin);
+$manager->add($administrator);

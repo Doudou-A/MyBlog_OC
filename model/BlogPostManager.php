39fda2cc32467e1acff02bacb9c2adec9839ec9f
Doudou <adeldoudou1996@gmail.com>
2019-09-11 12:35:52 +0200

    Update BlogPostManager.php

---
+++ model/BlogPostManager.php
@@ -0,0 +1,75 @@
+<?php
+class BlogPostManager
+{
+	private $_db;
+
+	public function __construct($db)
+	{
+		$this->setDB($db);
+	}
+
+	public function add(BlogPost $blogp)
+	{
+		$q = $this->_db->prepare('INSERT INTO blogpost(title, châpo, content, dateCreated) VALUES (:title, :châpo, :content, NOW())');
+
+		$q->bindValue(':title', $blogp->title(), PDO::PARAM_STR);
+		$q->bindValue(':châpo', $blogp->châpo(), PDO::PARAM_STR);
+		$q->bindValue(':content', $blogp->content(), PDO::PARAM_STR;)
+
+		$q->execute();
+	}
+
+	public function delete(BlogPost $blogp)
+	{
+		$this->_db->exec('DELETE FROM blogPost WHERE id = '.$blogp->id());
+	}
+
+	public function get($id)
+	{
+		$id = (int) $id;
+		$q = $this->_db->query('SELECT id, title, châpo, content, dateLastUpdate, dateCreated FROM blogPost WHERE id='.$id);
+		$data = $q->detch(PDO::FETCH_ASSOC);
+
+		return new BlogPost($data);
+	}
+
+	public function getList()
+	{
+		$blogp = [];
+
+		$q = $this->_db)->query('SELECT id, title, châpo, content, dateLastUpdate, dateCreated FROM blogPost ORDER BY title');
+		
+		while ($data = $q->fetch(PDO::FETCH_ASSOC))
+		{
+			$blogp[] = new BlogPost($data);
+		}
+
+		return $blogp;
+	}
+
+	public function update(BlogPost $blogp)
+	{
+		$q = $this->_db->prepare('UPDATE blogp SET title = :title, châpo = :châpo, content = :content, dateLastUpdate = :dateLastUpdate WHERE id = :id');
+
+		$q->bindValue(':title', $blogp->title(), PDO::PARAM_STR);
+		$q->bindValue(':châpo', $blogp->châpo(), PDO::PARAM_STR);
+		$q->bindValue(':content', $blogp->content(), PDO::PARAM_STR);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

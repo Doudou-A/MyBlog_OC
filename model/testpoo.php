37fde6268e783cd84dc63947fc4372e7a959d980
Doudou <adeldoudou1996@gmail.com>
2019-09-11 10:21:38 +0200

    Update testpoo.php

--- model/testpoo.php
+++ model/testpoo.php
@@ -6,14 +6,18 @@ function chargerClasse($class)
 
 spl_autoload_register('chargerClasse'); // On enregistre la fonction en autoload pour qu'elle soit appelée dès qu'on instanciera une classe non déclarée.
 
-$com = new Comment([
+/*$com = new Comment([
 'pseudo' => 'yehhhh',
 'content' => 'fhuzeihezuezhhufezui'
+]);*/
+
+$id = new Administrator([
+'id' => 2
 ]);
 
 $db = new PDO('mysql:host=localhost;dbname=poo;charset=utf8', 'root', 'root');
 
-$manager = new CommentManager($db);
+$manager = new AdministratorManager($db);
 
-$manager->add($com);
+$manager->get($id);
 

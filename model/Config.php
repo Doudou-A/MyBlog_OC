d5a327cc955520de54dd704b57f01b0733014802
Doudou <adeldoudou1996@gmail.com>
2019-09-18 05:36:02 +0200

    Update Config.php

--- model/Config.php
+++ model/Config.php
@@ -1,9 +1,3 @@
 <?php
 
-private $db;
-
-function dbConnect()
-{
-	$db = new PDO('mysql:host=localhost;dbname=poo;charset=utf8', 'root', 'root');
-	return $db;
-}
+$db = new PDO('mysql:host=localhost;dbname=poo;charset=utf8', 'root', 'root');

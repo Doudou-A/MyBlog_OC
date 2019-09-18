229f72fb11b9962ea5cf9c8870775583893e228d
Doudou <adeldoudou1996@gmail.com>
2019-09-18 07:22:11 +0200

    Update AdministratorManager.php

--- model/AdministratorManager.php
+++ model/AdministratorManager.php
@@ -10,7 +10,7 @@ class AdministratorManager
 
 	public function add(Administrator $admin)
 	{
-		$q = $this->_db->prepare('INSERT INTO administrator(email, name, firstName, password) VALUES(:email, :name, :firstName, :password)');
+		$q = $this->_db->prepare('INSERT INTO Administrator(email, name, firstName, password) VALUES(:email, :name, :firstName, :password)');
 
 		$q->bindValue(':email', $admin->email(), PDO::PARAM_STR);
 		$q->bindValue(':name', $admin->name(), PDO::PARAM_STR);
@@ -22,14 +22,14 @@ class AdministratorManager
 
 	public function delete(Administrator $admin)
 	{
-		$this->_db->exec('DELETE FROM administrator WHERE id = '.$admin->id());
+		$this->_db->exec('DELETE FROM Administrator WHERE id = '.$admin->id());
 	}
 
 	public function get($id)
 	{
 		$id = (int) $id;
 
-		$q = $this->_db->query('SELECT id, email, name, firstName, password FROM administrator WHERE id = '.$id);
+		$q = $this->_db->query('SELECT id, email, name, firstName, password FROM Administrator WHERE id = '.$id);
 		$data = $q->fetch(PDO::FETCH_ASSOC);
 
 		return new Administrator($data);
@@ -39,7 +39,7 @@ class AdministratorManager
 	{
 		$admin = [];
 
-		$q = $this->_db->query('SELECT id, email, name, firstName From administrator Order BY name');
+		$q = $this->_db->query('SELECT id, email, name, firstName From Administrator Order BY name');
 
 		while ($data = $q->fetch(PDO::FETCH_ASSOC))
 		{

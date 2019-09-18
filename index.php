5cd1dc8e83898b5f2988d5bd560d88b9816318aa
Doudou <adeldoudou1996@gmail.com>
2019-09-18 05:29:41 +0200

    Update index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+require('controller/controller.php');
+
+try 
+	{
+		function chargerClasse($class)
+	{
+	  require $class . '.php'; 
+	}
+
+	spl_autoload_register('chargerClasse'); 
+
+	$controller = new Controller;
+
+	if(!empty($_GET['action']))
+	{
+		$action = $_GET['action'];
+		if (method_exists($controller, $action))
+		{
+			$controller->$action();
+		}
+		else
+		{
+			throw new Exeption("Error Processing Request");
+		}
+	}
+	else
+	{
+		$controller->index();
+	}
+}
+
+catch(Exeption $e)
+{
+	die('Erreur : '.$e->getMessage());
+}

549736748846f50c623466ce5215346fe24ee305
Doudou <adeldoudou1996@gmail.com>
2019-09-18 07:15:02 +0200

    Update controller.php

--- controller/controller.php
+++ controller/controller.php
@@ -6,6 +6,27 @@ class Controller
 	{
 		require('view/indexView.php');
 	}
+
+	public function registrationView()
+	{
+		require('view/registrationView.php');
+	}
+
+	function formRegistration()
+	{
+		require('model/Config.php');
+
+		$manager = new AdministratorManager($db);
+
+		$admin = new Administrator([
+		'email' => $_POST['email'],
+		'name' =>  $_POST['name'],
+		'firstName' =>  $_POST['firstName'],
+		'password' =>  $_POST['password'],
+		]);
+
+		$manager->add($admin);
+	}
 }
 /*
 function index()

66367c4668358d20db1a1d318e150bcfbae7fe58
Doudou <adeldoudou1996@gmail.com>
2019-09-18 05:29:51 +0200

    Create template.php

---
+++ view/template.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title><?= $title ?></title>
+		<link rel="stylesheet" href="style.css" />
+		<script type="text/javascript" scr="public/style.js"></script>
+	</head>
+
+	<body>
+		<?= $content ?>
+	</body>
+</html>

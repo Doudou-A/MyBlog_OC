0d4cf04e2f6c922f354fcf993116e9944b8556de
Doudou <adeldoudou1996@gmail.com>
2019-09-18 05:34:21 +0200

    Update testpoo.php

--- model/testpoo.php
+++ model/testpoo.php
@@ -6,6 +6,7 @@ function chargerClasse($class)
 
 spl_autoload_register('chargerClasse'); // On enregistre la fonction en autoload pour qu'elle soit appelée dès qu'on instanciera une classe non déclarée.
 
+$db = new dbConnect();
 
 $admin = new Administrator([
 'email' => 'Meeeee',

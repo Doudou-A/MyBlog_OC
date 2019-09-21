9514da6ec290528e7fca9c824ff836d2ca660838
Doudou <adeldoudou1996@gmail.com>
2019-09-21 11:11:35 +0200

    Update template.php

--- view/template.php
+++ view/template.php
@@ -3,7 +3,8 @@
 	<head>
 		<meta charset="utf-8" />
 		<title><?= $title ?></title>
-		<link rel="stylesheet" href="style.css" />
+		<link rel="stylesheet" href="public/bootstrap.css" />
+		<link rel="stylesheet" href="public/bootstrap.min.css" />
 		<script type="text/javascript" scr="public/style.js"></script>
 	</head>
 

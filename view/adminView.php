c34a6ab2ac624de2453e958ca3bb745e9f08ca3a
Doudou <adeldoudou1996@gmail.com>
2019-09-21 05:01:09 +0200

    Update adminView.php

--- view/adminView.php
+++ view/adminView.php
@@ -1,10 +1,14 @@
 <?php $title = 'Administration'; ?>
 
-<?php ob_start(); ?>
+<?php 
+ob_start();
+session_start();
+ ?>
+
 <p>Gérer les Blogs Posts</p>
-<a href="index.php?action=addBlogPostView">Ajouter un Blog Post</a>
-<a href="index.php?action=updateBlogPostView">Modifier un Blog Post</a>
-<a href="index.php?action=deleteBlogPostView">Supprimer un Blog Post</a>
+<a href="index.php?action=blogPostAddView">Ajouter un Blog Post</a>
+<a href="index.php?action=blogPostUpdateView">Modifier un Blog Post</a>
+<a href="index.php?action=blogPostDeleteView">Supprimer un Blog Post</a>
 <?php $content = ob_get_clean(); ?>
 
 <?php require('template.php'); ?>

3e1114d4c355379e3731c0af2312ea918a73d679
Doudou <adeldoudou1996@gmail.com>
2019-09-22 11:35:57 +0200

    Create blogPostGetView.php

---
+++ view/blogPostGetView.php
@@ -0,0 +1,43 @@
+<?php $title = 'Ajouter un Blog Post'; ?>
+
+<?php 
+ob_start(); 
+session_start();
+?>
+
+<h1>Modifier un Blog Post</h1>
+<?php
+		$k = 1;
+		while($k<=$NumberId)
+		{
+?>		
+		<div class="col-lg-12 d-flex">
+			<div class="col-lg-2">
+				<?=$blogp[$k]->idBlogPost();?>		
+			</div>
+			<div class="col-lg-2">
+				<?=$blogp[$k]->title();?>
+			</div>
+			<div class="col-lg-2">
+				<?=$blogp[$k]->chapo();?>
+			</div>
+			<div class="col-lg-2"> 
+				<?=$blogp[$k]->content();?>	
+			</div>
+			<div class="col-lg-2">
+				<a href="index.php?action=blogPostUpdateView&amp;actions=<?=$blogp[$k]->idBlogPost();?>" >Modifier ce Blog Post</a>
+			</div>
+			<div class="col-lg-2">
+				<a href="index.php?action=blogPostDelete&amp;actions=<?=$blogp[$k]->idBlogPost();?>" >Supprimer ce Blog Post</a>
+			</div>
+		</div>
+<?php
+			$k++;
+?>
+</br>
+<?php
+		}
+?>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>

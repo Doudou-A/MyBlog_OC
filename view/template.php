01d7fdb27933ec1556aecd322784beab09fe385f
Doudou <adeldoudou1996@gmail.com>
2019-11-03 09:18:07 +0100

    p5

--- view/template.php
+++ view/template.php
@@ -3,10 +3,13 @@
 	<head>
 		<meta charset="utf-8" />
 		<title><?= $title ?></title>
-		<link rel="stylesheet" href="public/css/bootstrap.css" />
-		<link rel="stylesheet" href="public/css/mdb.css" />
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="/public/style.js"></script>
-		<nav class="navbar navbar-expand-md navbar-dark green d-flex flex-wrap">
+		<link rel="stylesheet" href="public/css/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="public/css/mdb.css" />
+		<nav class="navbar navbar-expand-md navbar-dark green d-flex flex-wrap m-0">
 			<div class="w-100 green p-2 text-white">
 				<?php if (isset($_SESSION['firstName'])) : ?>
 		  		Utilisateur : <?=$_SESSION['name']?> <?=$_SESSION['firstName']?>
@@ -16,16 +19,16 @@
 		    <span class="navbar-toggler-icon"></span>
 		  </button>
 		  <div class="collapse navbar-collapse" id="navbarCollapse">
-		    <ul class="navbar-nav mr-auto">
-		      <li class="nav-item active">
+		    <ul class="navbar-nav mr-auto col-10 d-flex text-justify-around">
+		      <li class="nav-item col-4 active">
 		        <a class="nav-link" href="index.php">Accueil <span class="sr-only">(current)</span></a>
 		      </li>
-		      <li class="nav-item">
+		      <li class="nav-item col-4">
 		        <a class="nav-link" href="Tous-les-Articles.html">Tous les Articles</a>
 		      </li>
 		      <?php if(isset($_SESSION['firstName'])) : ?>
-		       <li class="nav-item">
-		        <div class="nav-link admin-btn" id="Administration">Administration</div>
+		       <li class="nav-item admin-btn col-4 mt-3">
+		        <div class="nav-link" id="Administration">Administration</div>
 				  <div class="d-flex flex-column aqua-gradient">
 					<btn style="display: none;" class="admin-btn p-3 font-weight-bold animated bounceIn" id="BlogsPosts" >Articles
 						<div style="display: none;" id="BlogsPostsLinks">
